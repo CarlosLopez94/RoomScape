c0d6e64251135423c2d38beae363ac13d6c58633
Carlos <carloslopezrodriguez94@gmail.com>
2017-12-25 02:12:09 +0100

    First Commit

---
+++ Source/RoomScape/RoomScapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoomScapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMSCAPE_API ARoomScapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
