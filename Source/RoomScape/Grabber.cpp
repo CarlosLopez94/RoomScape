6cd8ab338264738743aa2e0effad36b5bb03422b
Carlos <carloslopezrodriguez94@gmail.com>
2017-12-28 15:30:14 +0100

    chairs and table are in physhics channel now, pawn has a line trace and it reports collisions

--- Source/RoomScape/Grabber.cpp
+++ Source/RoomScape/Grabber.cpp
@@ -8,8 +8,7 @@
 
 
 // Sets default values for this component's properties
-UGrabber::UGrabber()
-{
+UGrabber::UGrabber() {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
@@ -19,8 +18,7 @@ UGrabber::UGrabber()
 
 
 // Called when the game starts
-void UGrabber::BeginPlay()
-{
+void UGrabber::BeginPlay() {
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Component says MURRAY!"));
@@ -29,23 +27,36 @@ void UGrabber::BeginPlay()
 
 
 // Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Get player view point this tick
+	///Get player view point this tick
 	FVector playerViewPointLocation;
 	FRotator playerViewPointRotation;
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(playerViewPointLocation, playerViewPointRotation);
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(playerViewPointLocation,
+		playerViewPointRotation);
 
-	//UE_LOG(LogTemp, Warning, TEXT("%s location: (%f,%f,%f) AND rotation: (%f,%f,%f)"), *(GetOwner()->GetName()), playerViewPointLocation.X, playerViewPointLocation.Y, playerViewPointLocation.Z, playerViewPointRotation.Roll, playerViewPointRotation.Pitch, playerViewPointRotation.Yaw);
 
-	//Draw a red trace in the world
+	///Draw a red trace in the world
 	FVector lineTraceEnd = playerViewPointLocation + playerViewPointRotation.Vector()*reach;
-	DrawDebugLine(GetWorld(),playerViewPointLocation, lineTraceEnd,FColor::Red,false,0.f,0.f,10.f);
+	DrawDebugLine(GetWorld(), playerViewPointLocation, lineTraceEnd, FColor::Red, false, 0.f, 0.f, 10.f);
 
-	//Ray-cast out the reach distance
+	///Setup query parameters
+	FCollisionQueryParams traceParameters = FCollisionQueryParams(FName(TEXT("")), false, GetOwner());
 
-	//See what we hit
+	///Line-trace: Ray-cast out the reach distance
+	FHitResult hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		hit,
+		playerViewPointLocation,
+		lineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		traceParameters);
+
+	///See what we hit
+	if (hit.Actor!=nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("%s can grabber %s"),*GetOwner()->GetName(),*hit.Actor->GetName());
+	}
+
 }
 
